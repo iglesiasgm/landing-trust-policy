cfb482a3cfd88b9bcb2c7c5e18c9bb558acd1369
Gabriel Iglesias <gabriel@crea.ar>
2025-06-30 14:38:39 -0300

    fix: ruta absoluta

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,7 +2,7 @@ const PDFViewer = () => {
   return (
     <div className="max-w-4xl mx-auto my-6 px-4">
       <iframe
-        src="public/docs/pdf_example.pdf"
+        src="/docs/trust-policy.pdf"
         className="w-full h-[80vh] border rounded shadow"
         title="Trust Policy PDF"
       />
