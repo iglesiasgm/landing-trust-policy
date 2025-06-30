b94d539003bf325b42b9834d7892a3bc42896277
Gabriel Iglesias <gabriel@crea.ar>
2025-06-30 16:40:13 -0300

    buildeo 4

--- src/components/HeroBackground.tsx
+++ src/components/HeroBackground.tsx
@@ -5,7 +5,7 @@ const HeroBackground = () => {
     <div
       className="w-full h-[900px] bg-cover bg-center absolute top-0 left-0 z-[-10] pointer-events-none  inset-0 bg-gradient-to-b from-black/30 to-white/0"
       style={{
-        backgroundImage: `url('public/building.png')`,
+        backgroundImage: `url('/building.png')`,
       }}
       
     />
