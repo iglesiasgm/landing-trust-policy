0f7cf3bc239498f43d0ad9ff9bd5aeb25cb419ef
Gabriel Iglesias <gabriel@crea.ar>
2025-06-30 14:30:05 -0300

    mover el pdf example a carpeta public

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -2,7 +2,7 @@ const PDFViewer = () => {
   return (
     <div className="max-w-4xl mx-auto my-6 px-4">
       <iframe
-        src="src/docs/pdf_example.pdf"
+        src="public/docs/pdf_example.pdf"
         className="w-full h-[80vh] border rounded shadow"
         title="Trust Policy PDF"
       />
