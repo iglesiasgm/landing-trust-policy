2c8676b46d89ac0849af81b5e0972310c249d64b
Gabriel Iglesias <gabriel@crea.ar>
2025-06-30 14:34:08 -0300

    buildeo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-white shadow-md py-6 px-4">
       <h1 className="text-4xl font-geo font-bold text-center text-purple-500" style={{ fontFamily: 'Geologica, sans-serif' }}>
-        NB CREA S.A.U
+        NB CREA SAU
       </h1>
       <h3 className="text-xl pt-3 font-geo font-bold text-center text-purple-500" style={{ fontFamily: 'Geologica, sans-serif' }}>
         Política de Privacidad
